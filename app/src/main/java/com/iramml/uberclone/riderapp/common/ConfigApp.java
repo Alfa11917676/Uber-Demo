386e848506d674ca005d9f62fe4279f2ccf869dc
ARNAB RAY <59610398+Arnab11917676@users.noreply.github.com>
2021-04-27 16:03:25 +0530

    Done

---
+++ app/src/main/java/com/iramml/uberclone/riderapp/common/ConfigApp.java
@@ -0,0 +1,10 @@
+package com.iramml.uberclone.riderapp.common;
+
+public class ConfigApp {
+    public static final double baseFare = 2.55;
+    public static final double timeRate = 0.35;
+    public static final double distanceRate = 1.75;
+    public static final String CLOUD_MESSAGING_SERVER_GEY = "AAAAPAiSIAE:APA91bEYYGwSccLcmSzYidhirMzGNX5jFpIvhqCGTdekXk-F7E1Sr-pTc6jPMuFAbNuzmfBgtcgAC-oA_9EkshkKcLYHfo6glxI26WmJVQwkWFH94Nrejyi36IR0s6b6TudDQQBGm2OA";
+    public static final String GOOGLE_API_KEY = "AIzaSyCkFAgYeIH6YjTgMGm5jaraLglmPf5MyjM";
+
+}
